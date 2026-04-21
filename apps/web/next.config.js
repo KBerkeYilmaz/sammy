dd0e9b229da63bd42168d7a7fc544a36f1e20c49
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2026-04-21 14:50:34 +0300

    fix: conditional dynamic import instead of relying t3-env's skipValidation internal behavior.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,9 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+if (!process.env.SKIP_ENV_VALIDATION) {
+  await import("./src/env.js");
+}
 import path from "path";
 import { fileURLToPath } from "url";
 
